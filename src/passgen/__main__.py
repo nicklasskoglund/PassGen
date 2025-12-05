c7dca6d699ca1b09ef85198de72ac7d67a516e1c
nicklasskoglund <nicklas.jobb@gmail.com>
2025-12-05 21:44:16 +0100

    added functions: print_header, print_menu, run_app, error handling. Tested and working

---
+++ src/passgen/__main__.py
@@ -0,0 +1,8 @@
+# src/passgen/__main__.py
+
+from .main import run_app       # import the main application function
+
+
+if __name__ == '__main__':
+    # this block runs when you execute: python -m passgen
+    run_app()
